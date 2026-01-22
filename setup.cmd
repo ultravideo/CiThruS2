023e87cc9e29ce504de3d1d303adefa442030797
Arttu Leppäaho <arttu.leppaaho@tuni.fi>
2026-01-22 13:25:39 +0200

    Improved Windows setup script

--- setup.cmd
+++ setup.cmd
@@ -12,13 +12,55 @@ set CITHRUS_CONTENT_VER=24_11_2025
 
 set ROOT_DIR=%~dp0
 
-:: Get PowerShell
+call :powershellsetup
+
+chdir /d %ROOT_DIR%
+if exist temp rmdir temp /s /q
+mkdir temp
+
+echo Setting up CiThruS2...
+
+:: Set up individual dependencies one by one
+call :contentsetup
+:: AirSim is not available for UE 5.6, skip for now
+::call :airsimsetup
+call :dlsssetup
+call :fsrsetup
+call :impostorbakersetup
+call :kvazaarsetup
+call :openhevcsetup
+call :uvgrtpsetup
+call :fpngsetup
+
+if exist temp\Yasm (
+	echo Cleaning up Yasm files...
+	rmdir temp\Yasm /s /q
+)
+
+echo CiThruS2 setup was successful! Next, open CiThruS.uproject in Unreal Engine 5 to access the simulation environment.
+
+if exist temp rmdir temp /s /q
+chdir /d %ROOT_DIR%
+pause
+exit /b 0
+
+:setupfailed
+if exist temp rmdir temp /s /q
+chdir /d %ROOT_DIR%
+pause
+exit /b 1
+
+:powershellsetup
 set powershell=powershell
 where powershell > nul 2>&1
 if errorlevel 1 (
 	echo PowerShell is required by this script: please install it.
 	goto :setupfailed
 )
+
+goto :eof
+
+:visualstudiosetup
 
 :: Get VS Command Prompt
 if "%VisualStudioVersion%" == "" (
@@ -31,29 +73,19 @@ if "%VisualStudioVersion%" == "" (
 	if errorlevel 1 goto :vslocatefailed
 )
 
-goto :setupstart
+goto :eof
 
 :vslocatefailed
 echo Could not find x64 Native Tools Command Prompt for VS 2022 automatically: Please open it manually and run this script in it.
 goto :setupfailed
 
-:setupstart
-chdir /d %ROOT_DIR%
-if exist temp rmdir temp /s /q
-mkdir temp
-
-echo Setting up CiThruS2...
-
 :contentsetup
-
-:: Skip content download when using internal repository
-:: goto :airsimsetup
-
 if exist Content (
 	echo CiThruS content already found at Content, skipping... ^(Please delete the folder manually if you'd like to redownload it.^)
-	goto :airsimsetup
+	goto :eof
 )
 
+:: Download
 echo Downloading CiThruS2 content...
 %powershell% -command "(New-Object Net.WebClient).DownloadFile('https://ultravideo.fi/sim_env/cithrus2_sim_env_content_%CITHRUS_CONTENT_VER%.zip', 'temp\CiThruS2_content.zip')" || goto :contentdownloadfailed
 echo Extracting CiThruS2 content...
@@ -71,24 +103,21 @@ if not exist Saved\Config\WindowsEditor\EditorPerProjectUserSettings.ini (
 	) > Saved\Config\WindowsEditor\EditorPerProjectUserSettings.ini
 )
 
+:: Finish
 echo Finished setting up CiThruS2 content.
-goto :airsimsetup
+goto :eof
 
 :contentdownloadfailed
 echo Failed to download CiThruS2 content!
 goto :setupfailed
 
 :airsimsetup
-
-:: AirSim is not available for UE 5.6, skip for now
-goto :dlsssetup
-
-:: AirSim setup
 if exist Plugins\AirSim (
 	echo AirSim already found at Plugins\AirSim, skipping... ^(Please manually delete the folder if you'd like to reinstall it.^)
-	goto :dlsssetup
+	goto :eof
 )
 
+:: Download
 echo Downloading AirSim (from Colosseum)...
 git clone https://github.com/ArttuLeppaaho/Colosseum temp\Colosseum
 
@@ -96,9 +125,11 @@ cd temp\Colosseum
 git submodule update --init
 cd ..\..
 
+:: Build
 echo Running AirSim setup...
 call temp\Colosseum\build.cmd --Release || goto :airsimbuildfailed
 
+:: Copy the plugin files
 echo Copying AirSim plugin files...
 if not exist Plugins mkdir Plugins
 
@@ -110,12 +141,13 @@ robocopy temp\Colosseum\Unreal\Plugins Plugins /e
 :: Another patch to remove an unused folder which was accidentally included in the Colosseum release and prevents it from compiling
 if exist Plugins\AirSim\Source\AssetCode rmdir Plugins\AirSim\Source\AssetCode /s /q
 
+:: Finish
 echo Cleaning up AirSim files...
 rmdir temp\Colosseum-%AIRSIM_VER% /s /q
 
 echo AirSim successfully set up.
 
-goto :dlsssetup
+goto :eof
 
 :airsimdownloadfailed
 echo Failed to download AirSim!
@@ -128,9 +160,10 @@ goto :setupfailed
 :dlsssetup
 if exist Plugins\DLSS (
 	echo Nvidia DLSS already found at Plugins\DLSS, skipping... ^(Please delete the folder manually if you'd like to reinstall it.^)
-	goto :fsrsetup
+	goto :eof
 )
 
+:: Download
 echo Downloading Nvidia DLSS...
 %powershell% -command "(New-Object Net.WebClient).DownloadFile('https://dlss.download.nvidia.com/uebinarypackages/%DLSS_VER%.zip', 'temp\DLSS.zip')" || goto :dlssdownloadfailed
 echo Extracting Nvidia DLSS...
@@ -138,15 +171,18 @@ mkdir temp\DLSS
 %powershell% -command "Expand-Archive -Path temp\DLSS.zip -DestinationPath temp\DLSS"
 del temp\DLSS.zip /q
 
+:: Copy the plugin files
 mkdir Plugins\DLSS
 
 robocopy temp\DLSS\Plugins\DLSS Plugins\DLSS /e
 
+:: Finish
+echo Cleaning up DLSS files...
 rmdir temp\DLSS /s /q
 
 echo Nvidia DLSS successfully set up.
 
-goto :fsrsetup
+goto :eof
 
 :dlssdownloadfailed
 echo Failed to download Nvidia DLSS!
@@ -155,9 +191,10 @@ goto :setupfailed
 :fsrsetup
 if exist Plugins\FSR4 if exist Plugins\FSR4MovieRenderPipeline (
 	echo AMD FSR4 already found at Plugins\FSR4 and Plugins\FSR4MovieRenderPipeline, skipping... ^(Please delete the folders manually if you'd like to reinstall it.^)
-	goto :impostorbakersetup
+	goto :eof
 )
 
+:: Download
 echo Downloading AMD FSR 4...
 %powershell% -command "(New-Object Net.WebClient).DownloadFile('https://gpuopen.com/download-Unreal-Engine-FSR4-plugin/', 'temp\FSR.zip')" || goto :fsrdownloadfailed
 echo Extracting AMD FSR 4...
@@ -168,6 +205,7 @@ del temp\FSR.zip /q
 if exist Plugins\FSR4 rmdir Plugins\FSR4 /s /q
 if exist Plugins\FSR4MovieRenderPipeline rmdir Plugins\FSR4MovieRenderPipeline /s /q
 
+:: Copy the correct plugin files (the zip file includes multiple versions)
 mkdir Plugins\FSR4
 mkdir Plugins\FSR4MovieRenderPipeline
 
@@ -176,11 +214,13 @@ for /d %%i in (temp\UE-FSR-*) do set "FSR_FOLDER_NAME=%%i"
 robocopy "%FSR_FOLDER_NAME%\FSR4-%FSR_VER%\FSR4" Plugins\FSR4 /e
 robocopy "%FSR_FOLDER_NAME%\FSR4-%FSR_VER%\FSR4MovieRenderPipeline" Plugins\FSR4MovieRenderPipeline /e
 
+:: Finish
+echo Cleaning up FSR files...
 rmdir %FSR_FOLDER_NAME% /s /q
 
 echo AMD FSR 4 successfully set up.
 
-goto :impostorbakersetup
+goto :eof
 
 :fsrdownloadfailed
 echo Failed to download FSR 4!
@@ -189,53 +229,30 @@ goto :setupfailed
 :impostorbakersetup
 if exist Plugins\ImpostorBaker-master (
 	echo ImpostorBaker already found at Plugins\ImpostorBaker-master, skipping... ^(Please delete the folder manually if you'd like to reinstall it.^)
-	goto :librarysetup
+	goto :eof
 )
 
+:: Download
 echo Downloading ImpostorBaker...
 %powershell% -command "(New-Object Net.WebClient).DownloadFile('https://github.com/ictusbrucks/ImpostorBaker/archive/refs/heads/master.zip', 'temp\impostorbaker.zip')" || goto :impostorbakerdownloadfailed
 echo Extracting ImpostorBaker...
 %powershell% -command "Expand-Archive -Path temp\ImpostorBaker.zip -DestinationPath Plugins"
 del temp\ImpostorBaker.zip /q
 
+:: Finish
 echo ImpostorBaker successfully set up.
 
-goto :librarysetup
+goto :eof
 
 :impostorbakerdownloadfailed
 echo Failed to download ImpostorBaker!
 goto :setupfailed
 
-:librarysetup
-:: Get the compiler version and paste it in Unreal Engine's build configuration to make sure the libraries and CiThruS are compiled with the same compiler. Otherwise they will fail to link
-for /f "delims=" %%i in ('cl 2^>^&1 ^| findstr "[0-9][0-9]*\.[0-9][0-9]*\.[0-9][0-9]*"') do set COMPILER_VER=%%i
-for /f "delims=" %%i in ('%powershell% -command "('%COMPILER_VER%' | Select-String -Pattern '\.[0-9][0-9]*\.[0-9][0-9]*').Matches.Value"') do set COMPILER_VER=%%i
-
-if not exist Saved mkdir Saved
-if not exist Saved\UnrealBuildTool mkdir Saved\UnrealBuildTool
-if not exist Saved\UnrealBuildTool\BuildConfiguration.xml (
-	(
-		echo ^<^?xml version="1.0" encoding="utf-8" ^?^>
-		echo ^<Configuration xmlns="https://www.unrealengine.com/BuildConfiguration"^>
-		echo ^	^<WindowsPlatform^>
-		echo ^		^<CompilerVersion^>14.00.00000^</CompilerVersion^>
-		echo ^		^<Compiler^>VisualStudio2022^</Compiler^>
-		echo ^	^</WindowsPlatform^>
-		echo ^	^<VCProjectFileGenerator^>
-		echo ^		^<Version^>VisualStudio2022^</Version^>
-		echo ^	^</VCProjectFileGenerator^>
-		echo ^</Configuration^>
-	) > Saved\UnrealBuildTool\BuildConfiguration.xml
+:yasmsetup
+if exist temp\Yasm (
+	goto :eof
 )
 
-%powershell% -command "((Get-Content -path Saved/UnrealBuildTool/BuildConfiguration.xml -Raw) -replace '14\.[0-9]+\.[0-9]+','14%COMPILER_VER%') | Set-Content -Path Saved/UnrealBuildTool/BuildConfiguration.xml"
-
-if not exist ThirdParty\Kvazaar goto :yasmsetup
-if not exist ThirdParty\OpenHEVC goto :yasmsetup
-
-goto :uvgrtpsetup
-
-:yasmsetup
 :: Yasm is needed to build Kvazaar and OpenHEVC
 echo Downloading Yasm...
 %powershell% -command "(New-Object Net.WebClient).DownloadFile('http://www.tortall.net/projects/yasm/releases/vsyasm-%YASM_VER%-win64.zip', 'temp\Yasm.zip')" || goto :yasmdownloadfailed
@@ -246,7 +263,7 @@ del temp\Yasm.zip /q
 :: Kvazaar finds Yasm through the PATH environment variable
 set PATH=%PATH%;%ROOT_DIR%temp\Yasm\
 
-goto :kvazaarsetup
+goto :eof
 
 :yasmdownloadfailed
 echo Failed to download Yasm!
@@ -255,22 +272,32 @@ goto :setupfailed
 :kvazaarsetup
 if exist ThirdParty\Kvazaar (
 	echo Kvazaar already found at ThirdParty\Kvazaar, skipping... ^(Please delete the folder manually if you'd like to reinstall it.^)
-	goto :openhevcsetup
+	goto :eof
 )
 
+:: Set up dependencies
+call :visualstudiosetup
+call :yasmsetup
+
+:: Download
 echo Downloading Kvazaar...
 %powershell% -command "(New-Object Net.WebClient).DownloadFile('https://github.com/ultravideo/kvazaar/archive/v%KVAZAAR_VER%.zip', 'temp\Kvazaar.zip')" || goto :kvazaardownloadfailed
 echo Extracting Kvazaar...
 %powershell% -command "Expand-Archive -Path temp\Kvazaar.zip -DestinationPath temp -Force"
 del temp\Kvazaar.zip /q
 
+:: Build
 echo Building Kvazaar...
 
 :: Change the build configuration of Kvazaar to disable assembly output: otherwise Kvazaar doesn't work with CiThruS
 %powershell% -command "((Get-Content -path temp\kvazaar-%KVAZAAR_VER%\build\C_Properties.props -Raw) -replace 'AssemblyAndSourceCode','NoListing') | Set-Content -Path temp\kvazaar-%KVAZAAR_VER%\build\C_Properties.props"
 
+:: Change the build configuration of Kvazaar to disable whole program optimization. It keeps causing this issue whenever people use different VS versions https://developercommunity.visualstudio.com/t/cannot-build-after-vs-update-link-error/1348830
+%powershell% -command "((Get-Content -path temp\kvazaar-%KVAZAAR_VER%\build\Release_Optimizations.props -Raw) -replace '<WholeProgramOptimization>true</WholeProgramOptimization>','<WholeProgramOptimization>false</WholeProgramOptimization>') | Set-Content -Path temp\kvazaar-%KVAZAAR_VER%\build\Release_Optimizations.props"
+
 msbuild temp\kvazaar-%KVAZAAR_VER%\build\kvazaar_VS2015.sln /p:Configuration=Release /p:Platform=x64 /p:PlatformToolset=v143 /p:WindowsTargetPlatformVersion=10.0 || goto :kvazaarbuildfailed
 
+:: Copy results
 mkdir ThirdParty\Kvazaar\Lib
 mkdir ThirdParty\Kvazaar\Include
 
@@ -278,12 +305,13 @@ robocopy temp\kvazaar-%KVAZAAR_VER% ThirdParty\Kvazaar LICENSE
 robocopy temp\kvazaar-%KVAZAAR_VER%\build\x64-Release-libs ThirdParty\Kvazaar\Lib /e
 robocopy temp\kvazaar-%KVAZAAR_VER%\src ThirdParty\Kvazaar\Include kvazaar.h
 
+:: Finish
 echo Cleaning up Kvazaar files...
 rmdir temp\kvazaar-%KVAZAAR_VER% /s /q
 
 echo Kvazaar successfully set up.
 
-goto :openhevcsetup
+goto :eof
 
 :kvazaardownloadfailed
 echo Failed to download Kvazaar!
@@ -296,9 +324,14 @@ goto :setupfailed
 :openhevcsetup
 if exist ThirdParty\OpenHEVC (
 	echo OpenHEVC already found at ThirdParty\OpenHEVC, skipping... ^(Please delete the folder manually if you'd like to reinstall it.^)
-	goto :uvgrtpsetup
+	goto :eof
 )
 
+:: Set up dependencies
+call :visualstudiosetup
+call :yasmsetup
+
+:: Download
 echo Downloading OpenHEVC...
 %powershell% -command "(New-Object Net.WebClient).DownloadFile('https://github.com/OpenHEVC/openHEVC/archive/refs/heads/%OPENHEVC_VER%.zip', 'temp\OpenHEVC.zip')" || goto :openhevcdownloadfailed
 echo Extracting OpenHEVC...
@@ -308,7 +341,7 @@ del temp\OpenHEVC.zip /q
 :: Building OpenHEVC is broken on Windows, needs to be patched
 echo Patching OpenHEVC...
 :: Set updated CMake version and enable C11
-%powershell% -command "((Get-Content -path temp\openHEVC-%OPENHEVC_VER%\CMakeLists.txt -Raw) -replace 'cmake_minimum_required \(VERSION 2\.8\)',\""cmake_minimum_required (VERSION 3.5)`nset (CMAKE_C_STANDARD 11)\"") | Set-Content -Path temp\openHEVC-%OPENHEVC_VER%\CMakeLists.txt"
+%powershell% -command "((Get-Content -path temp\openHEVC-%OPENHEVC_VER%\CMakeLists.txt -Raw) -replace 'cmake_minimum_required \(VERSION 2\.8\)',\""cmake_minimum_required (VERSION 3.10)`nset (CMAKE_C_STANDARD 11)\"") | Set-Content -Path temp\openHEVC-%OPENHEVC_VER%\CMakeLists.txt"
 :: m library doesn't exist on Windows and isn't needed anyway. Replace with explicitly enabling C11 atomics
 %powershell% -command "((Get-Content -path temp\openHEVC-%OPENHEVC_VER%\CMakeLists.txt -Raw) -replace 'target_link_libraries\(LibOpenHevcWrapper m\)',\""if (MSVC)`n`ttarget_compile_options(LibOpenHevcWrapper PRIVATE /experimental:c11atomics)`nendif()\"") | Set-Content -Path temp\openHEVC-%OPENHEVC_VER%\CMakeLists.txt"
 :: These definitions break MSVC and aren't needed anyway
@@ -333,11 +366,13 @@ echo Patching OpenHEVC...
 :: ATOMIC_VAR_INIT is deprecated
 %powershell% -command "((Get-Content -path temp\openHEVC-%OPENHEVC_VER%\libavutil\cpu.c -Raw) -replace 'static atomic_int cpu_flags = ATOMIC_VAR_INIT\(-1\);','static atomic_int cpu_flags = -1;') | Set-Content -Path temp\openHEVC-%OPENHEVC_VER%\libavutil\cpu.c"
 
+:: Build
 echo Building OpenHEVC...
 
 cmake temp\openHEVC-%OPENHEVC_VER% -Btemp\openHEVC-%OPENHEVC_VER%\build -DYASM_EXECUTABLE="%ROOT_DIR%temp\Yasm\vsyasm.exe" -DYASM_FOUND=True -DENABLE_STATIC=True
 msbuild temp\openHEVC-%OPENHEVC_VER%\build\openHEVC.sln /target:LibOpenHevcWrapper /p:Configuration=Release /p:Platform=x64 /p:PlatformToolset=v143 /p:WindowsTargetPlatformVersion=10.0 || goto :openhevcbuildfailed
 
+:: Copy results
 mkdir ThirdParty\OpenHEVC\Lib
 mkdir ThirdParty\OpenHEVC\Include
 
@@ -345,12 +380,13 @@ robocopy temp\openHEVC-%OPENHEVC_VER% ThirdParty\OpenHEVC COPYING.LGPLv2.1
 robocopy temp\openHEVC-%OPENHEVC_VER%\build\Release ThirdParty\OpenHEVC\Lib /e
 robocopy temp\openHEVC-%OPENHEVC_VER%\gpac\modules\openhevc_dec ThirdParty\OpenHEVC\Include openHevcWrapper.h
 
+:: Finish
 echo Cleaning up OpenHEVC files...
 rmdir temp\openHEVC-%OPENHEVC_VER% /s /q
 
 echo OpenHEVC successfully set up.
 
-goto :uvgrtpsetup
+goto :eof
 
 :openhevcdownloadfailed
 echo Failed to download OpenHEVC!
@@ -363,15 +399,20 @@ goto :setupfailed
 :uvgrtpsetup
 if exist ThirdParty\uvgRTP (
 	echo uvgRTP already found at ThirdParty\uvgRTP, skipping... ^(Please delete the folder manually if you'd like to reinstall it.^)
-	goto :fpngsetup
+	goto :eof
 )
 
+:: Set up dependencies
+call :visualstudiosetup
+
+:: Download
 echo Downloading uvgRTP...
 %powershell% -command "(New-Object Net.WebClient).DownloadFile('https://github.com/ultravideo/uvgRTP/archive/v%UVGRTP_VER%.zip', 'temp\uvgRTP.zip')" || goto :uvgrtpdownloadfailed
 echo Extracting uvgRTP...
 %powershell% -command "Expand-Archive -Path temp\uvgRTP.zip -DestinationPath temp -Force"
 del temp\uvgRTP.zip /q
 
+:: Build
 echo Building uvgRTP...
 
 mkdir temp\uvgRTP-%UVGRTP_VER%\build
@@ -379,6 +420,7 @@ cmake temp\uvgRTP-%UVGRTP_VER% -Btemp\uvgRTP-%UVGRTP_VER%\build
 
 msbuild temp\uvgRTP-%UVGRTP_VER%\build\uvgrtp.sln /p:Configuration="Release" /p:Platform=x64 /p:PlatformToolset=v143 /p:WindowsTargetPlatformVersion=10.0 || goto :uvgrtpbuildfailed
 
+:: Copy results
 mkdir ThirdParty\uvgRTP\Lib
 mkdir ThirdParty\uvgRTP\Include
 
@@ -386,11 +428,12 @@ robocopy temp\uvgRTP-%UVGRTP_VER% ThirdParty\uvgRTP COPYING
 robocopy temp\uvgRTP-%UVGRTP_VER%\build\Release ThirdParty\uvgRTP\Lib /e
 robocopy temp\uvgRTP-%UVGRTP_VER%\include\uvgrtp ThirdParty\uvgRTP\Include
 
+:: Finish
 echo Cleaning up uvgRTP files...
 rmdir temp\uvgRTP-%UVGRTP_VER% /s /q
 
 echo Finished setting up uvgRTP.
-goto :fpngsetup
+goto :eof
 
 :uvgrtpdownloadfailed
 echo Failed to download uvgRTP!
@@ -403,20 +446,26 @@ goto :setupfailed
 :fpngsetup
 if exist ThirdParty\fpng (
 	echo fpng already found at ThirdParty\fpng, skipping... ^(Please delete the folder manually if you'd like to reinstall it.^)
-	goto :setupsuccess
+	goto :eof
 )
 
+:: Set up dependencies
+call :visualstudiosetup
+
+:: Download
 echo Downloading fpng...
 %powershell% -command "(New-Object Net.WebClient).DownloadFile('https://github.com/richgel999/fpng/archive/refs/tags/v%FPNG_VER%.zip', 'temp\fpng.zip')" || goto :fpngdownloadfailed
 echo Extracting fpng...
 %powershell% -command "Expand-Archive -Path temp\fpng.zip -DestinationPath temp -Force"
 del temp\fpng.zip /q
 
+:: Build
 echo Building fpng...
 
 cl /c /Fotemp\fpng-%FPNG_VER%\fpng.obj /D_MT /D_DLL temp\fpng-%FPNG_VER%\src\fpng.cpp || goto :fpngbuildfailed
 lib /OUT:temp\fpng-%FPNG_VER%\fpng.lib temp\fpng-%FPNG_VER%\fpng.obj || goto :fpngbuildfailed
 
+:: Copy results
 mkdir ThirdParty\fpng\Lib
 mkdir ThirdParty\fpng\Include
 
@@ -424,11 +473,12 @@ robocopy temp\fpng-%FPNG_VER% ThirdParty\fpng README.md
 robocopy temp\fpng-%FPNG_VER%\src ThirdParty\fpng\Include fpng.h
 robocopy temp\fpng-%FPNG_VER% ThirdParty\fpng\Lib fpng.lib
 
+:: Finish
 echo Cleaning up fpng files...
 rmdir temp\fpng-%FPNG_VER% /s /q
 
 echo Finished setting up fpng.
-goto :setupsuccess
+goto :eof
 
 :fpngdownloadfailed
 echo Failed to download fpng!
@@ -437,22 +487,3 @@ goto :setupfailed
 :fpngbuildfailed
 echo Failed to build fpng!
 goto :setupfailed
-
-:setupsuccess
-if exist temp\Yasm (
-	echo Cleaning up Yasm files...
-	rmdir temp\Yasm /s /q
-)
-
-echo CiThruS2 setup was successful! Next, open CiThruS.uproject in Unreal Engine 5 to access the simulation environment.
-
-if exist temp rmdir temp /s /q
-chdir /d %ROOT_DIR%
-pause
-exit /b 0
-
-:setupfailed
-if exist temp rmdir temp /s /q
-chdir /d %ROOT_DIR%
-pause
-exit /b 1
