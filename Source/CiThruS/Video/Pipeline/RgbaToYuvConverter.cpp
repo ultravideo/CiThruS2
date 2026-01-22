0359deeeb76931dafb37b3e7838b0ee499f9cd29
Arttu Leppäaho <arttu.leppaaho@tuni.fi>
2026-01-22 13:45:58 +0200

    Cleaner RGB<->YUV conversions + versions without SSE 4.1

--- Source/CiThruS/Video/Pipeline/RgbaToYuvConverter.cpp
+++ Source/CiThruS/Video/Pipeline/RgbaToYuvConverter.cpp
@@ -10,10 +10,27 @@
 #include <stdexcept>
 
 RgbaToYuvConverter::RgbaToYuvConverter(const uint16_t& frameWidth, const uint16_t& frameHeight)
-	: outputFrameWidth_(frameWidth), outputFrameHeight_(frameHeight)
+	: outputFrameWidth_(frameWidth), outputFrameHeight_(frameHeight), converterFunction_(nullptr), initialized_(false)
 {
     uint32_t outputSize = outputFrameWidth_ * outputFrameHeight_ * 3 / 2;
     outputData_ = new uint8_t[outputSize];
+
+    if (outputFrameWidth_ % 2 != 0 || outputFrameHeight_ % 2 != 0)
+    {
+        throw std::runtime_error("The width and height of YUV 4:2:0 data must be divisible by 2");
+    }
+
+#ifdef CITHRUS_SSE41_AVAILABLE
+    if (converterFunction_ == nullptr && frameWidth % 16 == 0 && frameHeight % 16 == 0)
+    {
+        converterFunction_ = &RgbaToYuvConverter::RgbaToYuvSse41;
+    }
+#endif // CITHRUS_SSE41_AVAILABLE
+
+    if (converterFunction_ == nullptr)
+    {
+        converterFunction_ = &RgbaToYuvConverter::RgbaToYuvDefault;
+    }
 
     GetInputPin<0>().SetAcceptedFormats({ "rgba", "bgra" });
     GetOutputPin<0>().SetFormat("yuv420");
@@ -41,134 +58,165 @@ void RgbaToYuvConverter::Process()
         return;
     }
 
-	RgbaToYuvSse41(inputData, &outputData_, outputFrameWidth_, outputFrameHeight_);
+	(this->*converterFunction_)(inputData, &outputData_, outputFrameWidth_, outputFrameHeight_);
 }
 
-void RgbaToYuvConverter::RgbaToYuvSse41(const uint8_t* input, uint8_t** output, int width, int height)
+void RgbaToYuvConverter::RgbaToYuvDefault(const uint8_t* input, uint8_t** output, int width, int height)
 {
-#ifdef CITHRUS_SSE41_AVAILABLE
-    // This efficiently converts pixels from RGBA to YUV 4:2:0 by using SSE 4.1 instructions to process multiple values simultaneously
-
-    const int r_mul_y[4] = {  76,  76,  76,  76 };
-    const int r_mul_u[4] = { -43, -43, -43, -43 };
-    const int r_mul_v[4] = { 127, 127, 127, 127 };
-
-    const int g_mul_y[4] = {  150,  150,  150,  150 };
-    const int g_mul_u[4] = {  -84,  -84,  -84,  -84 };
-    const int g_mul_v[4] = { -106, -106, -106, -106 };
-
-    const int b_mul_y[4] = {  29,  29,  29,  29 };
-    const int b_mul_u[4] = { 127, 127, 127, 127 };
-    const int b_mul_v[4] = { -21, -21, -21, -21 };
-
-    const __m128i r_y = _mm_loadu_si128((__m128i const*)r_mul_y);
-    const __m128i r_u = _mm_loadu_si128((__m128i const*)r_mul_u);
-    const __m128i r_v = _mm_loadu_si128((__m128i const*)r_mul_v);
-
-    const __m128i g_y = _mm_loadu_si128((__m128i const*)g_mul_y);
-    const __m128i g_u = _mm_loadu_si128((__m128i const*)g_mul_u);
-    const __m128i g_v = _mm_loadu_si128((__m128i const*)g_mul_v);
-
-    const __m128i b_y = _mm_loadu_si128((__m128i const*)b_mul_y);
-    const __m128i b_u = _mm_loadu_si128((__m128i const*)b_mul_u);
-    const __m128i b_v = _mm_loadu_si128((__m128i const*)b_mul_v);
-
-    const int mini[4] = { 0, 0, 0, 0 };
-    const int maxi[4] = { 255, 255, 255, 255 };
-
-    const int chroma_offset[4] = { 255 * 255, 255 * 255, 255 * 255, 255 * 255 };
-
-    const uint8_t* in = input;
-
-    const __m128i min_val = _mm_loadu_si128((__m128i const*)mini);
-    const __m128i max_val = _mm_loadu_si128((__m128i const*)maxi);
-    const __m128i cr_offset = _mm_loadu_si128((__m128i const*)chroma_offset);
-
-    uint8_t* out_y = &(*output)[0];
-    uint8_t* out_u = &(*output)[width * height];
-    uint8_t* out_v = &(*output)[width * height * 5 / 4];
-
-    __m128i shufflemask_r;
-    __m128i shufflemask_g;
-    __m128i shufflemask_b;
-
-    std::string inputFormat = GetInputPin<0>().GetFormat();
-
-    if (inputFormat == "rgba")
+    if (!initialized_)
     {
-        shufflemask_r = _mm_set_epi8(-1, -1, -1, 12, -1, -1, -1, 8, -1, -1, -1, 4, -1, -1, -1, 0);
-        shufflemask_g = _mm_set_epi8(-1, -1, -1, 13, -1, -1, -1, 9, -1, -1, -1, 5, -1, -1, -1, 1);
-        shufflemask_b = _mm_set_epi8(-1, -1, -1, 14, -1, -1, -1, 10, -1, -1, -1, 6, -1, -1, -1, 2);
-    }
-    else if (inputFormat == "bgra")
-    {
-        shufflemask_r = _mm_set_epi8(-1, -1, -1, 14, -1, -1, -1, 10, -1, -1, -1, 6, -1, -1, -1, 2);
-        shufflemask_g = _mm_set_epi8(-1, -1, -1, 13, -1, -1, -1, 9, -1, -1, -1, 5, -1, -1, -1, 1);
-        shufflemask_b = _mm_set_epi8(-1, -1, -1, 12, -1, -1, -1, 8, -1, -1, -1, 4, -1, -1, -1, 0);
-    }
-    else
-    {
-        throw std::runtime_error("Unsupported format");
-    }
+        initialized_ = true;
 
-    __m128i shufflemask = _mm_set_epi8(-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 12, 8, 4, 0);
-    __m128i shufflemask_chroma = _mm_set_epi8(-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 8, 0);
+        std::string inputFormat = GetInputPin<0>().GetFormat();
 
-    // Calculate Y 4 pixels at a time
-    for (int32_t i = 0; i < width * height << 2; i += 16)
-    {
-        // Load 16 bytes (4 pixels)
-        const __m128i a = _mm_loadu_si128((__m128i const*)in);
-        in += 16;
-
-        __m128i r = _mm_shuffle_epi8(a, shufflemask_r);
-        __m128i g = _mm_shuffle_epi8(a, shufflemask_g);
-        __m128i b = _mm_shuffle_epi8(a, shufflemask_b);
-
-        __m128i res_y = _mm_max_epi32(min_val, _mm_min_epi32(max_val, _mm_srai_epi32(_mm_add_epi32(_mm_add_epi32(_mm_mullo_epi32(r, r_y), _mm_mullo_epi32(g, g_y)), _mm_mullo_epi32(b, b_y)), 8)));
-        __m128i temp_y = _mm_shuffle_epi8(res_y, shufflemask);
-
-        memcpy(out_y, &temp_y, 4);
-        out_y += 4;
-    }
-
-    // Calculate U and V 8 pixels at a time
-    for (int i = 0; i < width * height * 4; i += 2 * width * 4)
-    {
-        for (int j = i; j < i + width * 4; j += 4 * 4)
+        if (inputFormat == "rgba")
         {
-            const __m128i a = _mm_loadu_si128((__m128i const*) & input[j]);
-            const __m128i a2 = _mm_loadu_si128((__m128i const*) & input[j + width * 4]);
-
-            __m128i r = _mm_shuffle_epi8(a, shufflemask_r);
-            __m128i g = _mm_shuffle_epi8(a, shufflemask_g);
-            __m128i b = _mm_shuffle_epi8(a, shufflemask_b);
-
-            __m128i r2 = _mm_shuffle_epi8(a2, shufflemask_r);
-            __m128i g2 = _mm_shuffle_epi8(a2, shufflemask_g);
-            __m128i b2 = _mm_shuffle_epi8(a2, shufflemask_b);
-
-            __m128i r_sum = _mm_add_epi32(r, r2);
-            __m128i g_sum = _mm_add_epi32(g, g2);
-            __m128i b_sum = _mm_add_epi32(b, b2);
-
-            __m128i res_u_b = _mm_add_epi32(_mm_add_epi32(_mm_add_epi32(_mm_mullo_epi32(r_sum, r_u), _mm_mullo_epi32(g_sum, g_u)), _mm_mullo_epi32(b_sum, b_u)), cr_offset);
-            __m128i res_u_tmp = _mm_max_epi32(min_val, _mm_min_epi32(max_val, _mm_srai_epi32(res_u_b, 9)));
-
-            __m128i res_u = _mm_srai_epi32(_mm_add_epi32(_mm_shuffle_epi32(res_u_tmp, 0xb1), res_u_tmp), 1);
-            __m128i temp_u = _mm_shuffle_epi8(res_u, shufflemask_chroma);
-            memcpy(out_u, &temp_u, 2);
-            out_u += 2;
-
-            __m128i res_v_b = _mm_add_epi32(_mm_add_epi32(_mm_add_epi32(_mm_mullo_epi32(r_sum, r_v), _mm_mullo_epi32(g_sum, g_v)), _mm_mullo_epi32(b_sum, b_v)), cr_offset);
-            __m128i res_v_tmp = _mm_max_epi32(min_val, _mm_min_epi32(max_val, _mm_srai_epi32(res_v_b, 9)));
-
-            __m128i res_v = _mm_srai_epi32(_mm_add_epi32(_mm_shuffle_epi32(res_v_tmp, 0xb1), res_v_tmp), 1);
-            __m128i temp_v = _mm_shuffle_epi8(res_v, shufflemask_chroma);
-            memcpy(out_v, &temp_v, 2);
-            out_v += 2;
+            rOffset_ = 0;
+            gOffset_ = 1;
+            bOffset_ = 2;
+        }
+        else if (inputFormat == "bgra")
+        {
+            rOffset_ = 2;
+            gOffset_ = 1;
+            bOffset_ = 0;
+        }
+        else
+        {
+            throw std::runtime_error("Unsupported format");
         }
     }
 
-#endif // CITHRUS_SSE41_AVAILABLE
+    for (int i = 0; i < height / 2; i++)
+    {
+        for (int j = 0; j < width / 2; j++)
+        {
+            const std::array<uint8_t, 4>& rgbaTopLeft     = *(reinterpret_cast<const std::array<uint8_t, 4>*>(input) + ((i * 2 + 0) * width + (j * 2 + 0)));
+            const std::array<uint8_t, 4>& rgbaTopRight    = *(reinterpret_cast<const std::array<uint8_t, 4>*>(input) + ((i * 2 + 0) * width + (j * 2 + 1)));
+            const std::array<uint8_t, 4>& rgbaBottomLeft  = *(reinterpret_cast<const std::array<uint8_t, 4>*>(input) + ((i * 2 + 1) * width + (j * 2 + 0)));
+            const std::array<uint8_t, 4>& rgbaBottomRight = *(reinterpret_cast<const std::array<uint8_t, 4>*>(input) + ((i * 2 + 1) * width + (j * 2 + 1)));
+
+            // Calculate Y for 4 pixels in a square
+            int32_t yTopLeft     = R_Y * rgbaTopLeft[rOffset_]     + G_Y * rgbaTopLeft[gOffset_]     + B_Y * rgbaTopLeft[bOffset_];
+            int32_t yTopRight    = R_Y * rgbaTopRight[rOffset_]    + G_Y * rgbaTopRight[gOffset_]    + B_Y * rgbaTopRight[bOffset_];
+            int32_t yBottomLeft  = R_Y * rgbaBottomLeft[rOffset_]  + G_Y * rgbaBottomLeft[gOffset_]  + B_Y * rgbaBottomLeft[bOffset_];
+            int32_t yBottomRight = R_Y * rgbaBottomRight[rOffset_] + G_Y * rgbaBottomRight[gOffset_] + B_Y * rgbaBottomRight[bOffset_];
+
+            // Fixed point multiplication
+            (*output)[(i * 2 + 0) * width + (j * 2 + 0)] = std::max(CHROMA_MIN, std::min(CHROMA_MAX, yTopLeft     >> 8));
+            (*output)[(i * 2 + 0) * width + (j * 2 + 1)] = std::max(CHROMA_MIN, std::min(CHROMA_MAX, yTopRight    >> 8));
+            (*output)[(i * 2 + 1) * width + (j * 2 + 0)] = std::max(CHROMA_MIN, std::min(CHROMA_MAX, yBottomLeft  >> 8));
+            (*output)[(i * 2 + 1) * width + (j * 2 + 1)] = std::max(CHROMA_MIN, std::min(CHROMA_MAX, yBottomRight >> 8));
+
+            // Average U and V from 4 pixels in a square
+            int16_t rSum = rgbaTopLeft[rOffset_] + rgbaTopRight[rOffset_] + rgbaBottomLeft[rOffset_] + rgbaBottomRight[rOffset_];
+            int16_t gSum = rgbaTopLeft[gOffset_] + rgbaTopRight[gOffset_] + rgbaBottomLeft[gOffset_] + rgbaBottomRight[gOffset_];
+            int16_t bSum = rgbaTopLeft[bOffset_] + rgbaTopRight[bOffset_] + rgbaBottomLeft[bOffset_] + rgbaBottomRight[bOffset_];
+
+            // Fixed point multiplication
+            (*output)[i * width / 2 + j + width * height]         = std::max(CHROMA_MIN, std::min(CHROMA_MAX, (R_U * rSum + G_U * gSum + B_U * bSum + (CHROMA_MID << 10)) >> 10));
+            (*output)[i * width / 2 + j + width * height * 5 / 4] = std::max(CHROMA_MIN, std::min(CHROMA_MAX, (R_V * rSum + G_V * gSum + B_V * bSum + (CHROMA_MID << 10)) >> 10));
+        }
+    }
 }
+
+#ifdef CITHRUS_SSE41_AVAILABLE
+void RgbaToYuvConverter::RgbaToYuvSse41(const uint8_t* input, uint8_t** output, int width, int height)
+{
+    // This efficiently converts pixels from RGBA to YUV 4:2:0 by using SSE 4.1 instructions to process multiple values simultaneously
+
+    const __m128i* in = reinterpret_cast<const __m128i*>(input);
+
+    if (!initialized_)
+    {
+        initialized_ = true;
+
+        std::string inputFormat = GetInputPin<0>().GetFormat();
+
+        if (inputFormat == "rgba")
+        {
+            rShuffleMask_ = _mm_set_epi8(-1, -1, -1, 12, -1, -1, -1,  8, -1, -1, -1, 4, -1, -1, -1, 0);
+            gShuffleMask_ = _mm_set_epi8(-1, -1, -1, 13, -1, -1, -1,  9, -1, -1, -1, 5, -1, -1, -1, 1);
+            bShuffleMask_ = _mm_set_epi8(-1, -1, -1, 14, -1, -1, -1, 10, -1, -1, -1, 6, -1, -1, -1, 2);
+        }
+        else if (inputFormat == "bgra")
+        {
+            rShuffleMask_ = _mm_set_epi8(-1, -1, -1, 14, -1, -1, -1, 10, -1, -1, -1, 6, -1, -1, -1, 2);
+            gShuffleMask_ = _mm_set_epi8(-1, -1, -1, 13, -1, -1, -1,  9, -1, -1, -1, 5, -1, -1, -1, 1);
+            bShuffleMask_ = _mm_set_epi8(-1, -1, -1, 12, -1, -1, -1,  8, -1, -1, -1, 4, -1, -1, -1, 0);
+        }
+        else
+        {
+            throw std::runtime_error("Unsupported format");
+        }
+    }
+
+    uint8_t* yOut = *output;
+    uint8_t* uOut = *output + width * height;
+    uint8_t* vOut = *output + width * height * 5 / 4;
+
+    // Calculate Y 4 pixels at a time, 4x1 rectangle
+    for (int i = 0; i < width * height / 4; i++)
+    {
+        // Load 16 bytes (4 pixels)
+        const __m128i a = _mm_loadu_si128(in);
+        in++;
+
+        __m128i r = _mm_shuffle_epi8(a, rShuffleMask_);
+        __m128i g = _mm_shuffle_epi8(a, gShuffleMask_);
+        __m128i b = _mm_shuffle_epi8(a, bShuffleMask_);
+
+        // Fixed point multiplication
+        __m128i yRes = _mm_srai_epi32(_mm_add_epi32(_mm_add_epi32(_mm_mullo_epi32(r, rY_), _mm_mullo_epi32(g, gY_)), _mm_mullo_epi32(b, bY_)), 8);
+        __m128i yTemp = _mm_shuffle_epi8(_mm_max_epi32(minVal_, _mm_min_epi32(maxVal_, yRes)), shuffleMask_);
+
+        memcpy(yOut, &yTemp, 4);
+        yOut += 4;
+    }
+
+    // Calculate U and V 8 pixels at a time, 4x2 rectangle
+    for (int i = 0; i < height / 2; i++)
+    {
+        for (int j = 0; j < width / 4; j++)
+        {
+            const __m128i a = _mm_loadu_si128(reinterpret_cast<__m128i const*>(input) + i * width / 2 + j);
+            const __m128i a2 = _mm_loadu_si128(reinterpret_cast<__m128i const*>(input) + i * width / 2 + j + width / 4);
+
+            __m128i r = _mm_shuffle_epi8(a, rShuffleMask_);
+            __m128i g = _mm_shuffle_epi8(a, gShuffleMask_);
+            __m128i b = _mm_shuffle_epi8(a, bShuffleMask_);
+
+            __m128i r2 = _mm_shuffle_epi8(a2, rShuffleMask_);
+            __m128i g2 = _mm_shuffle_epi8(a2, gShuffleMask_);
+            __m128i b2 = _mm_shuffle_epi8(a2, bShuffleMask_);
+
+            // Sum vertically
+            __m128i rSum = _mm_add_epi32(r, r2);
+            __m128i gSum = _mm_add_epi32(g, g2);
+            __m128i bSum = _mm_add_epi32(b, b2);
+
+            // Fixed point multiplication
+            __m128i uResB = _mm_add_epi32(_mm_add_epi32(_mm_add_epi32(_mm_mullo_epi32(rSum, rU_), _mm_mullo_epi32(gSum, gU_)), _mm_mullo_epi32(bSum, bU_)), midVal_);
+            __m128i uResTemp = _mm_max_epi32(minVal_, _mm_min_epi32(maxVal_, _mm_srai_epi32(uResB, 9)));
+
+            // Sum horizontally
+            __m128i uRes = _mm_srai_epi32(_mm_add_epi32(_mm_shuffle_epi32(uResTemp, 0xB1), uResTemp), 1);
+            __m128i uTemp = _mm_shuffle_epi8(uRes, shuffleMaskChroma_);
+
+            memcpy(uOut, &uTemp, 2);
+            uOut += 2;
+
+            // Fixed point multiplication
+            __m128i vResB = _mm_add_epi32(_mm_add_epi32(_mm_add_epi32(_mm_mullo_epi32(rSum, rV_), _mm_mullo_epi32(gSum, gV_)), _mm_mullo_epi32(bSum, bV_)), midVal_);
+            __m128i vResTemp = _mm_max_epi32(minVal_, _mm_min_epi32(maxVal_, _mm_srai_epi32(vResB, 9)));
+
+            // Sum horizontally
+            __m128i vRes = _mm_srai_epi32(_mm_add_epi32(_mm_shuffle_epi32(vResTemp, 0xB1), vResTemp), 1);
+            __m128i vTemp = _mm_shuffle_epi8(vRes, shuffleMaskChroma_);
+
+            memcpy(vOut, &vTemp, 2);
+            vOut += 2;
+        }
+    }
+}
+#endif // CITHRUS_SSE41_AVAILABLE
--- Source/CiThruS/Video/Pipeline/RgbaToYuvConverter.h
+++ Source/CiThruS/Video/Pipeline/RgbaToYuvConverter.h
@@ -1,7 +1,6 @@
 #pragma once
 
 #include "Optional/Sse41.h"
-
 #include "PipelineFilter.h"
 
 // Converts RGBA or ABGR images to YUV 4:2:0
@@ -20,5 +19,57 @@ protected:
 	uint16_t outputFrameWidth_;
 	uint16_t outputFrameHeight_;
 
-	void RgbaToYuvSse41(const uint8_t* input, uint8_t** output, int width, int height);
+    void (RgbaToYuvConverter::*converterFunction_)(const uint8_t* input, uint8_t** output, int width, int height);
+
+    // Fixed point coefficients from ITU-R BT.601
+    const static int16_t R_Y =   76;
+    const static int16_t R_U =  -43;
+    const static int16_t R_V =  128;
+
+    const static int16_t G_Y =  150;
+    const static int16_t G_U =  -84;
+    const static int16_t G_V = -106;
+
+    const static int16_t B_Y =   29;
+    const static int16_t B_U =  128;
+    const static int16_t B_V =  -21;
+
+    const static int CHROMA_MIN = 0;
+    const static int CHROMA_MID = 128;
+    const static int CHROMA_MAX = 255;
+
+    uint8_t rOffset_;
+    uint8_t gOffset_;
+    uint8_t bOffset_;
+
+    bool initialized_;
+
+    void RgbaToYuvDefault(const uint8_t* input, uint8_t** output, int width, int height);
+
+#ifdef CITHRUS_SSE41_AVAILABLE
+    const __m128i rY_ = _mm_set_epi32(R_Y, R_Y, R_Y, R_Y);
+    const __m128i rU_ = _mm_set_epi32(R_U, R_U, R_U, R_U);
+    const __m128i rV_ = _mm_set_epi32(R_V, R_V, R_V, R_V);
+
+    const __m128i gY_ = _mm_set_epi32(G_Y, G_Y, G_Y, G_Y);
+    const __m128i gU_ = _mm_set_epi32(G_U, G_U, G_U, G_U);
+    const __m128i gV_ = _mm_set_epi32(G_V, G_V, G_V, G_V);
+
+    const __m128i bY_ = _mm_set_epi32(B_Y, B_Y, B_Y, B_Y);
+    const __m128i bU_ = _mm_set_epi32(B_U, B_U, B_U, B_U);
+    const __m128i bV_ = _mm_set_epi32(B_V, B_V, B_V, B_V);
+
+    const __m128i minVal_ = _mm_set_epi32(CHROMA_MIN,      CHROMA_MIN,      CHROMA_MIN,      CHROMA_MIN);
+    const __m128i midVal_ = _mm_set_epi32(CHROMA_MID << 9, CHROMA_MID << 9, CHROMA_MID << 9, CHROMA_MID << 9);
+    const __m128i maxVal_ = _mm_set_epi32(CHROMA_MAX,      CHROMA_MAX,      CHROMA_MAX,      CHROMA_MAX);
+
+    const __m128i shuffleMask_       = _mm_set_epi8(-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 12,  8,  4,  0);
+    const __m128i shuffleMaskChroma_ = _mm_set_epi8(-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,  8,  0);
+
+    __m128i rShuffleMask_;
+    __m128i gShuffleMask_;
+    __m128i bShuffleMask_;
+
+    void RgbaToYuvSse41(const uint8_t* input, uint8_t** output, int width, int height);
+#endif // CITHRUS_SSE41_AVAILABLE
 };
